d5f899be8ce46e800062a45b687e197fa29abde0
valeklubomir <valeklubomir@gmail.com>
2022-11-09 19:19:05 +0100

    TaskDelay updates

--- src/user_main.c
+++ src/user_main.c
@@ -491,6 +491,7 @@ void Main_OnEverySecond()
         sleepCount = 0;
         sleepTicks = 0;
 #ifdef PLATFORM_BEKEN
+        /***********************************/
         cnt = intc_get_handler_count();
         ADDLOGF_DEBUG("IRQ: %lu(%lu) FIQ: %lu(%lu) IRQ_CNT: %i\n", intc_get_irq_tick_count(), (intc_get_irq_tick_count() - lastIRQTicks),
                      intc_get_fiq_tick_count(), (intc_get_fiq_tick_count() - lastFIQTicks), cnt );
@@ -531,6 +532,7 @@ void Main_OnEverySecond()
             strcat(line,num);
         }
         ADDLOGF_DEBUG("IDIF: %s\n", line);
+        /***************************************/
 #endif
 	}
 
