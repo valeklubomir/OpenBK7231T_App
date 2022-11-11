e397c5308c69abb92cac7e05db7ab176db528dae
valeklubomir <valeklubomir@gmail.com>
2022-11-11 23:11:24 +0100

    Log level updates

--- src/user_main.c
+++ src/user_main.c
@@ -533,7 +533,7 @@ void Main_OnEverySecond()
             strcat(line,num);
         }
         ADDLOGF_DEBUG("IDIF: %s\n", line);
-        /******************************************/
+        /*******************************************/
 #endif
 	}
 
